6d751c028f14ec16a44e3bde703db6823ac657c3
Minhaj ur Rehman <72701898+minhaj6006@users.noreply.github.com>
2022-04-06 21:02:19 +0500

    Create Backend Email API.

---
+++ pages/api/mail.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+
+export default function handler(req, res) {
+  const { fromEmail, toEmail, subject, message } = req.body;
+
+  // create reusable transporter object using the default SMTP transport (configured in .env.local)
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: process.env.BOOTCAMP_MAIL_PORT,
+    auth: {
+      user: process.env.BOOTCAMP_MAIL_USER,
+      pass: process.env.BOOTCAMP_MAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: fromEmail,
+    to: toEmail,
+    subject,
+    html: message,
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(500).json({
+        message: "Error sending email",
+        error,
+      });
+    } else {
+      res.status(200).json({
+        message: "Email sent",
+        info,
+      });
+    }
+  });
+}
