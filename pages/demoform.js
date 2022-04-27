fe71e4cc2021c6116940a955cbba4387c6921df4
Minhaj ur Rehman <72701898+minhaj6006@users.noreply.github.com>
2022-04-27 05:54:50 +0500

    Refactor DemoForm.

--- pages/demoform.js
+++ pages/demoform.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 //formik used to create forms
 import { Formik, Form, Field } from "formik";
 //Yup is used for form validation
 import * as Yup from "yup";
+import { Country, City } from "country-state-city";
+import PhoneInput from "react-phone-number-input";
+import "react-phone-number-input/style.css";
 
 import DateTime from "../components/DateTime";
 import { TextField } from "../components/TextField";
 import styles from "../styles/demoform.module.css";
-import CountryContactPhone from "../components/CountryContactPhone";
+import style from "../styles/Country.module.css";
+// import CountryContactPhone from "../components/CountryContactPhone";
 
 function demoform() {
   const validate = Yup.object({
-    fullname: Yup.string(),
-    email: Yup.string().email(),
+    fullname: Yup.string().required("Required"),
+    email: Yup.string().email().required("Required"),
+    phone: Yup.string().required("Required"),
+    country: Yup.string().required("Required"),
+    city: Yup.string().required("Required"),
+    date: Yup.string().required("Required"),
+    time: Yup.string().required("Required"),
+  });
+
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  const [phone, setPhone] = useState("");
+
+  useEffect(() => {}, [country]);
+
+  const countryList = Country.getAllCountries("").map((country) => {
+    const { name, isoCode } = country;
+    return <option value={isoCode}>{name}</option>;
+  });
+
+  const cityList = City.getCitiesOfCountry(`${country}`).map((city) => {
+    const { name } = city;
+    return <option value={name}>{name}</option>;
   });
 
   return (
@@ -21,12 +46,22 @@ function demoform() {
         initialValues={{
           fullname: "",
           email: "",
-          countryDetails: { country: "", city: "", phone: "" },
+          country: "",
+          city: "",
+          phone: "",
           timing: "",
         }}
         validationSchema={validate}
         onSubmit={(values) => {
-          console.log(values);
+          const data = {
+            fullname: values.fullname,
+            email: values.email,
+            country: country,
+            city: city,
+            phone: phone,
+            timing: values.timing,
+          };
+          console.log(data);
         }}
       >
         {(formik) => (
@@ -36,26 +71,56 @@ function demoform() {
                 <h1>Demo Form</h1>
                 <Form>
                   <div className={styles.component}>
-                    <TextField label="Full Name" name="fullname" type="text" />
+                    <TextField label="Full Name:" name="fullname" type="text" />
                   </div>
                   <div className={styles.component}>
-                    <TextField label="Email" name="email" type="email" />
-                  </div>
-
-                  <div className={styles.component}>
-                    <CountryContactPhone
-                      name="countryDetails"
-                      as="select"
-                      label="Country Details"
-                    />
+                    <TextField label="Email:" name="email" type="email" />
                   </div>
 
                   <div className={styles.component}>
-                    <DateTime label="Select Date" name="date" type="date" />
+                    <div className={style.selectCountry}>
+                      <label>Select Country: </label>
+                      <select
+                        className={style.input}
+                        required
+                        value={country}
+                        onChange={(e) => setCountry(e.target.value)}
+                      >
+                        <option>-- Select --</option>
+                        {countryList}
+                      </select>
+                    </div>
+                    <div className={style.selectCountry}>
+                      <label className={style.input}>Select City: </label>
+                      <select
+                        className={style.input}
+                        required
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                      >
+                        <option>Select Country First</option>
+                        {cityList}
+                      </select>
+                    </div>
+                    <div>
+                      <label>Phone Number: </label>
+                      <PhoneInput
+                        required
+                        international
+                        defaultCountry={country}
+                        placeholder="Enter phone number"
+                        value={phone}
+                        onChange={setPhone}
+                      />
+                    </div>
                   </div>
 
                   <div className={styles.component}>
-                    <label>Timing</label>
+                    <DateTime label="Select Date:" name="date" type="date" />
+                  </div>
+
+                  <div className={styles.component}>
+                    <label>Timing:</label>
                     <br></br>
                     <div>
                       <Field name="timing" value="10:00 AM" type="radio" />
